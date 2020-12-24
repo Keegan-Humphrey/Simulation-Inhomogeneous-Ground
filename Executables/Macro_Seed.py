d691cf9a5039ee9cb47940055cadb95a9ff76223
Keegan-Humphrey <76496709+Keegan-Humphrey@users.noreply.github.com>
2020-12-23 21:11:02 -0500

    Update Macro_Seed.py

--- Executables/Macro_Seed.py
+++ Executables/Macro_Seed.py
@@ -12,10 +12,18 @@ from joblib import load
 
 Events = load('Events.joblib')
 
-r = open('r2.in','w+') #beware!!! using the same seed for real and sky
+seed = ''.join(random.choices(string.digits, k=10)) + ' \n'
+
+f = open('Seed_History.txt','a+')
+
+f.write(seed)
+
+f.close()
+
+r = open('r2.in','w+') 
     
 r.writelines(['/run/initialize \n \n', \
-              '/SetSeed/set {} \n \n'.format(''.join(random.choices(string.digits, k=10))), \
+              '/SetSeed/set {} \n \n'.format(seed), \
               '/gun/particle mu- \n \n', \
               '#/vis/scene/endOfEventAction accumulate \n \n', \
               '/run/beamOn {} \n \n'.format(Events)])
