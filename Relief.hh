4b1121e6db7c653e27a58ad3ec6268e920de21c1
Keegan-Humphrey <76496709+Keegan-Humphrey@users.noreply.github.com>
2020-12-23 20:36:14 -0500

    Create Relief.hh

---
+++ Relief.hh
@@ -0,0 +1,16 @@
+#include <array> 
+#include "G4SystemOfUnits.hh" 
+#include "G4UnitsTable.hh" 
+ 
+ 
+ 
+extern G4double Pnt_Sep; //seperation of data points
+ 
+extern int iaxis;
+extern int jaxis;
+
+extern G4double max_landscape_height;
+
+extern std::array<std::array<G4double, 60>, 60> Relief;
+
+extern std::array<std::array<G4double, 59>, 59> RMins;
